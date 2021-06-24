41abd3a6c71bbddef801b51afed57207b46edf38
AnnySad <anna.sadovskaya555@gmail.com>
2021-06-24 11:30:38 +0300

    Updates

---
+++ precache-manifest.90b5ac72f9814077489daca4ef2414cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c315b4bf40c7ce7477510caf080255c7",
+    "url": "/HW_HomeWork/index.html"
+  },
+  {
+    "revision": "00a0a1ddc8e8001ca242",
+    "url": "/HW_HomeWork/static/css/main.d788bdc1.chunk.css"
+  },
+  {
+    "revision": "319e77af47e35919ca16",
+    "url": "/HW_HomeWork/static/js/2.c450872d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW_HomeWork/static/js/2.c450872d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a0a1ddc8e8001ca242",
+    "url": "/HW_HomeWork/static/js/main.6387d66b.chunk.js"
+  },
+  {
+    "revision": "529bb9e76001243f251a",
+    "url": "/HW_HomeWork/static/js/runtime-main.589e6c82.js"
+  }
+]);
