0734b3160b5ad29ca9ece3d1c4e85ea28c6fad2e
AnnySad <anna.sadovskaya555@gmail.com>
2021-06-24 15:35:29 +0300

    fix 6

--- src/p2-homeworks/h5/pages/PreJunior.tsx
+++ src/p2-homeworks/h5/pages/PreJunior.tsx
@@ -3,17 +3,18 @@ import HW1 from '../../h1/HW1'
 import HW2 from '../../h2/HW2'
 import HW3 from '../../h3/HW3'
 import HW4 from '../../h4/HW4'
+import HW6 from "../../h6/HW6";
 
 function PreJunior() {
     return (
         <div>
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
+            <HW1/>
+            <HW2/>
+            <HW3/>
             <div className='size'>
                 <div className='title'>Наполнение для PreJunior</div></div>
-            {/*<HW4/>*/}
-            {/*<HW6/>*/}
+            <HW4/>
+            <HW6/>
 
         </div>
     )
--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,29 +10,27 @@ function HW6() {
         saveState<string>("editable-span-value", value);
     };
     const restore = () => {
-        // setValue();
+        const restoreValue = restoreState("editable-span-value", value)
+        setValue(restoreValue)
     };
 
     return (
         <div>
             <hr/>
             homeworks 6
-
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : "enter text..."}}
-                />
+            <div className='wrapper-hw6'>
+                <div>
+                    <SuperEditableSpan
+                        value={value}
+                        onChangeText={setValue}
+                        spanProps={{children: value ? undefined : "enter text..."}}
+                    />
+                </div>
+                <div className='button-hw6'>
+                    <SuperButton onClick={save}>save</SuperButton>
+                    <SuperButton onClick={restore}>restore</SuperButton>
+                </div>
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
         </div>
     );
 }
