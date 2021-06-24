c88e19dc139dafed0b55c8d9110f9218f005d6ff
AnnySad <anna.sadovskaya555@gmail.com>
2021-06-24 21:21:30 +0300

    11-polzunok

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,24 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './HW11.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+
+    const setMinValue = (newMinValue: number): void => {
+        setValue1(newMinValue)
+    }
+    const handleChange = (newValue: number | number[]): void => {
+        if (typeof newValue === "object") {
+            setValue1(newValue[0])
+            setValue2(newValue[1])
+        }
+    }
+
+    let value = [value1, value2]
+
 
     return (
         <div>
@@ -12,26 +26,24 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+            <div className={s.HW11}>
+                <div>
+                    <SuperRange
+                        onChangeRange={setMinValue}
+                        minValue={value1}
+                    />
+                </div>
+
+                <div>
+                    <span>{value1}</span>
+                    <SuperDoubleRange
+                        handleChange={handleChange}
+                        value={value}
+                    />
+                    <span>{value2}</span>
+                </div>
             </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
         </div>
     )
 }
